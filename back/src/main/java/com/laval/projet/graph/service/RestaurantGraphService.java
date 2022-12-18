1d567dc339f96bd2435b30a9ecd4469c70553432
Faustin <maniragenaf01@gmail.com>
2022-12-18 09:12:02 -0500

    CRUO operation with springbootApp Graph database

--- back/src/main/java/com/laval/projet/graph/service/RestaurantGraphService.java
+++ back/src/main/java/com/laval/projet/graph/service/RestaurantGraphService.java
@@ -1,16 +1,33 @@
+/**
+ * 
+ */
 package com.laval.projet.graph.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.laval.projet.graph.repositories.RestaurantGraphRepository;
 import com.laval.projet.models.Restaurant;
 
-public interface RestaurantGraphService {
+/**
+ * @author Faustin
+ *
+ */
+@Service
+public class RestaurantGraphService {
 
-	// Save operation
-   public  Restaurant saveRestaurantNode(Restaurant restaurantNode);
-   
-   // Read operation
-   List<Restaurant> fetchRestaurantNodeList();
-	
+	@Autowired
+	private RestaurantGraphRepository restaurantGraphRepository;
 
+	public Restaurant saveRestaurantNode(Restaurant restaurant) {
+		restaurantGraphRepository.save(restaurant);
+		return restaurant;
+	}
+
+	public List<Restaurant> fetchRestaurantNodeList() {
+		List<Restaurant> restaurants = restaurantGraphRepository.findAll();
+		return restaurants;
+	}
 }
